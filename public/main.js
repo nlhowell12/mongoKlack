bc939cc875d1206d01fb9031ff56a2c8fed56150
Nick Howell <nlhowell12@gmail.com>
2018-05-03 15:28:13 -0400

    pushing to heroku

--- public/main.js
+++ public/main.js
@@ -3,7 +3,7 @@ const messagesDiv = document.getElementById("messageslist");
 const feedback = document.getElementById("feedback");
 const textarea = document.getElementById("newmessage");
 const ding = new Audio('typewriter_ding.m4a');
-const socket = io.connect('http://localhost:3000')
+const socket = io.connect('https://git.heroku.com/kenzieslack.git')
 
 let name = window.prompt("Enter your name");
 // if they didn't type anything at the prompt, make up a random name
